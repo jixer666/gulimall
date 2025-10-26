5869dbd7e50fd044be219c681e2622c07c976793
lijunxi <2770063826@qq.com>
2025-10-26 21:16:26 +0800

    feat:新建认证模块

---
+++ gulimall-auth/src/main/java/com/ljx/auth/GulimallAuthApplication.java
@@ -0,0 +1,17 @@
+package com.ljx.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @Author: LiJunXi
+ * @Description:
+ * @Date: 2025-06-14  23:15
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class GulimallAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallAuthApplication.class, args);
+    }
+}
---
+++ gulimall-auth/src/main/java/com/ljx/auth/web/IndexController.java
@@ -0,0 +1,18 @@
+package com.ljx.auth.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+
+
+    @GetMapping({"/", "/index"})
+    public String getIndexPage(Model model) {
+
+
+        return "login";
+    }
+}
